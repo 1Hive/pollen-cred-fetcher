290bb2f2c5f92f1fb112a2ffbb5be6343c9c4110
Hernando Guzmán <hernando.guzman93@gmail.com>
2021-05-06 20:58:19 -0300

    Add types for mongoose Models

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,20 @@
+import { Document } from 'mongoose'
+
 export type PollenData = {
   accounts: any,
   credParticipants: any
+}
+
+export interface ICredParticipant extends Document {
+  credPerInterval: number[],
+  cred: number,
+  id: string
+}
+
+export interface IAccount extends Document {
+  identity: {
+    id: string,
+    subtype: string,
+    aliases: string[]
+  }
 }
